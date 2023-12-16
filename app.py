3eab9ff6103e536d9299de4366ebd1a0d50d61c5
Ademola <ademicho@gmail>
2023-12-16 21:55:37 +0000

    welcome message added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-
-
-
-
+'''
+    Unibuddy is dedicated to providing freshers with essential information about your the school, ensuring he/she navigates campus life with confidence.
+    
+'''
+print("Welcome to Unibuddy")
