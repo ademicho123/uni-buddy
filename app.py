18de89ac01ec514fa35a71c11a95d6753226719f
Ademola <ademicho@gmail>
2024-01-15 08:35:13 +0000

    questions and answers file added

--- app.py
+++ app.py
@@ -2,4 +2,9 @@
     Unibuddy is dedicated to providing freshers with essential information about your the school, ensuring he/she navigates campus life with confidence.
     
 '''
-print("Welcome to Unibuddy........dd")
+# Initialize a welcome message
+
+print("Welcome to Unibuddy - Your go-to companion for all things related to the school!")
+print("Please enter your credentials to get started!:")
+
+user_name: input("Please enter your name: ")
