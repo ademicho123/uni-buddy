51d6ea0bb96c3457c5512a65adcdfc888c473379
Ademola <ademicho@gmail>
2024-01-16 22:01:33 +0000

    updated the ipnynb

--- app.py
+++ app.py
@@ -7,4 +7,10 @@
 print("Welcome to Unibuddy - Your go-to companion for all things related to the school!")
 print("Please enter your credentials to get started!:")
 
-user_name: input("Please enter your name: ")
+user_name = input("Please enter your name: ")
+user_age = input("Please enter your age: ")
+user_color = input("Please enter your favourite colour: ")
+
+print(f'''
+    Welcome {user_name}!
+    ''')
